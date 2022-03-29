ab1388219a173e83a94b8f190dfee65988bf955b
LastDekay <77251869+LastDekay@users.noreply.github.com>
2022-03-29 15:56:14 -0400

    Added profile page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { UserProvider } from '@auth0/nextjs-auth0';
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const user = pageProps;
 
   return (
-    <UserProvider user = {user}>
+    <UserProvider user={user}>
       <Component {...pageProps} />
     </UserProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
